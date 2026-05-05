461cd7fabbd440fed554f339a2e93c7768574348
psell515-tech <psell515@gmail.com>
2026-05-05 09:06:31 -0400

    Update [key].js

--- functions/messages/[key].js
+++ functions/messages/[key].js
@@ -2,9 +2,7 @@ export async function onRequestDelete(context) {
   try {
     const key = context.params.key;
     await context.env.MESSAGES.delete(key);
-
     return new Response("Deleted", { status: 200 });
-
   } catch (err) {
     console.error(err);
     return new Response("Error deleting message", { status: 500 });
